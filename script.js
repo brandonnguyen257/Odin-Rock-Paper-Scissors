beb64a737a64772d6f33024ec754c6bfcd63622a
Brandon Nguyen <brandonnguyen257@gmail.com>
2023-07-15 12:06:58 -0700

    Add event listener to buttons to call playRound() method

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 
-function helloWorld() {
-    console.log("Hello World");
+const rockButton = document.getElementById("rock-button");
+const paperButton = document.getElementById('paper-button');
+const scissorsButton = document.getElementById('scissors-button');
+
+rockButton.addEventListener('click', () => playRound('rock'));
+paperButton.addEventListener('click', () => playRound('paper'));
+scissorsButton.addEventListener('click', () => playRound('scissors'));
+
+
+function playRound(humanInput) {
+    console.log(humanInput);
 }
