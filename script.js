40fc29384da8dc4a95fafa0f951023788e586713
Brandon Nguyen <brandonnguyen257@gmail.com>
2023-07-15 13:20:48 -0700

    Implement getRoundDecision() and update human or computer scare based off roundDecision

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+let humanScore = 0;
+let computerScore = 0;
+
 const GAME_OPTIONS = {
     ROCK: 'rock',
     PAPER: 'paper',
     SCISSORS: 'scissors'
+}
+
+const ROUND_DECISION = {
+    HUMAN_WIN: 'human',
+    COMPUTER_WIN: 'computer',
+    TIE: 'tie'
 }
 
 const rockButton = document.getElementById("rock-button");
@@ -15,7 +24,11 @@ scissorsButton.addEventListener('click', () => playRound(GAME_OPTIONS.SCISSORS))
 
 function playRound(humanInput) {
     const computerInput = getComputerGameInput();
-    console.log(humanInput, computerInput);
+    const roundDecision = getRoundDecision(humanInput, computerInput);
+    processRoundDecision(roundDecision);
+    console.log(humanInput, computerInput, roundDecision);
+    console.log(humanScore, computerScore);
+
 }
 
 function getComputerGameInput() {
@@ -30,5 +43,37 @@ function getComputerGameInput() {
             return GAME_OPTIONS.SCISSORS;
         default:
             return undefined;
+    }
+}
+
+function getRoundDecision(humanInput, computerInput) {
+    if (humanInput === computerInput) return ROUND_DECISION.TIE;
+
+    if(humanInput === GAME_OPTIONS.ROCK) {
+        return computerInput === GAME_OPTIONS.SCISSORS ? 
+        ROUND_DECISION.HUMAN_WIN : ROUND_DECISION.COMPUTER_WIN;
+    }
+
+    if(humanInput === GAME_OPTIONS.PAPER) {
+        return computerInput === GAME_OPTIONS.ROCK ? 
+        ROUND_DECISION.HUMAN_WIN : ROUND_DECISION.COMPUTER_WIN;
+    }
+
+    if(humanInput === GAME_OPTIONS.SCISSORS) {
+        return computerInput === GAME_OPTIONS.PAPER ? 
+        ROUND_DECISION.HUMAN_WIN : ROUND_DECISION.COMPUTER_WIN;
+    }
+}
+
+function processRoundDecision(roundDecision) {
+    switch (roundDecision) {
+        case ROUND_DECISION.HUMAN_WIN:
+            humanScore+=1;
+            break;
+        case ROUND_DECISION.COMPUTER_WIN:
+            computerScore+=1;
+            break;
+        default:
+            console.log('Tie Switch Case');
     }
 }
